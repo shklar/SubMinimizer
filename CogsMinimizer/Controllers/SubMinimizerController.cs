f101ffef74bc03fffd5cc90f4c844cba440cd9d8
maximsh <maximsh@microsoft.com>
2016-12-07 09:47:05 +0200

    Exception handling

--- CogsMinimizer/Controllers/SubMinimizerController.cs
+++ CogsMinimizer/Controllers/SubMinimizerController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CogsMinimizer.Shared;
+using Microsoft.ApplicationInsights;
 using Microsoft.Azure.Management.Authorization.Models;
 using Resource = CogsMinimizer.Shared.Resource;
 
@@ -14,12 +15,20 @@ namespace CogsMinimizer.Controllers
 {
     public class SubMinimizerController : Controller
     {
+
         protected override void OnException(ExceptionContext context)
         {
             context.ExceptionHandled = true;
             context.HttpContext.Response.Clear();
-            System.Diagnostics.Trace.TraceError($"{context.Exception.Message} {context.Exception.StackTrace}");
-            context.Result = RedirectToAction("Error", "Home", new { Exception = context.Exception.Message + " " + context.Exception.StackTrace});
+            Response.StatusCode = 500;
+
+            Exception excep = context.Exception;
+            var stack = excep.StackTrace ?? string.Empty;
+            var text = $"Exception happened. Message: {excep.Message} Stack: {stack}";
+
+            System.Diagnostics.Trace.TraceError(text);
+     
+            context.Result = RedirectToAction("Error", "Home", new { Exception = context.Exception.Message});
         }
     }
 }
