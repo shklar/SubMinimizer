4fa486a6059d86f929f752c0db1368cd7a381853
Maxim Shklar <shklar@users.noreply.github.com>
2019-03-07 15:03:54 +0200

    Replacing DB ADAL token cache with an in memory token cache (#88)
    * Replacing DB ADAL token cache with an in memory token cache.
    Removing Shared app config (wasn't used)
    
    * Fixed Evgeny's comments

---
+++ Shared/InMemoryTokenCache.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace CogsMinimizer.Shared
+{
+    /// <summary>
+    /// An in memory token cache to hold the ADAL Tokens
+    /// </summary>
+    public class InMemoryTokenCache
+    {
+        private static InMemoryTokenCache s_instance;
+        private static object s_lock = new object();
+
+        public static InMemoryTokenCache Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    lock (s_lock)
+                    {
+                        if (s_instance == null)
+                        {
+                            s_instance = new InMemoryTokenCache();
+                        }
+                    }
+                }
+                return s_instance;
+            }
+            private set
+            {
+                s_instance = value;
+            }
+        }
+
+        private InMemoryTokenCache()
+        {
+            PerUserTokenCacheList = new HashSet<PerUserTokenCache>();
+        }
+        public HashSet<PerUserTokenCache> PerUserTokenCacheList { get; set; }    
+    }
+}
